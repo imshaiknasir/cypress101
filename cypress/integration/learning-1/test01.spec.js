cea808dff3757d5ace7f52a274539c1aa08c3203
imshaiknasir <skfiroj1994@gmail.com>
2021-06-23 02:50:19 +0530

    changed viewport size

--- cypress/integration/learning-1/test01.spec.js
+++ cypress/integration/learning-1/test01.spec.js
@@ -1,5 +1,6 @@
 describe('suite 1',()=>{
     it('test1',()=>{
-        cy.visit('https://www.cleartrip.com/');
-    })
+        cy.visit('https://www.ixigo.com/');
+        cy.get('.u-v-align-top > .nav-list > :nth-child(2) > .nav-list-item').click();
+    });
 });
