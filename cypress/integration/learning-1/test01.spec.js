881236e1528d62c12971af3a2eb4eefd5a6b63a9
imshaiknasir <skfiroj1994@gmail.com>
2021-06-27 23:20:55 +0530

    changed viewports

--- cypress/integration/learning-1/test01.spec.js
+++ cypress/integration/learning-1/test01.spec.js
@@ -2,5 +2,6 @@ describe('suite 1',()=>{
     it('test1',()=>{
         cy.visit('https://www.ixigo.com/');
         cy.get('.u-v-align-top > .nav-list > :nth-child(2) > .nav-list-item').click();
+        
     });
 });
